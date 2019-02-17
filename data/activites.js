6314cf42e76409161b8904588c4b89ca81d868e3
Edward Feng <edward0414@gmail.com>
2019-02-17 01:42:04 -0500

    add data

---
+++ data/activites.js
@@ -0,0 +1,8 @@
+export const activites = [
+  { label: "Walking" },
+  { label: "Arm Curls" },
+  { label: "Pull Ups" },
+  { label: "Bench Press" },
+  { label: "Plank" },
+  { label: "Log Press" }
+];
